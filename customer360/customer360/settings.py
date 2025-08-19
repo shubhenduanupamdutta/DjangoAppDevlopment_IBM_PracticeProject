a40f855cf8487b2ccde8d7298c3cd6fa53941108
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2025-08-19 21:53:57 +0530

    Some more configurations

--- customer360/customer360/settings.py
+++ customer360/customer360/settings.py
@@ -31,7 +31,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-
+CSRF_TRUSTED_ORIGINS = ["https://*.cognitiveclass.ai"]
 # Application definition
 
 INSTALLED_APPS = [
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "customer360",
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATICFILES_DIRS = (BASE_DIR / "static",)
